f715d5bbcfeedd9718ca9ce8a5359e18b0444c58
Diego Gómez González <93382813+diegogomezgonza@users.noreply.github.com>
2024-03-01 21:34:44 +0100

    Estructura, sass y otras dependencias

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -2,9 +2,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-//include your index.scss file into the bundle
-import "../styles/index.css";
-
 //import your own components
 import Layout from "./layout";
 
