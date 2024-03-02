cc91b9538d6d3da8cf3ddca20b62564a83aeae91
Raul M <118123543+pillow3@users.noreply.github.com>
2024-03-02 21:06:31 +0000

    Add icons

--- src/front/js/pages/home.jsx
+++ src/front/js/pages/home.jsx
@@ -3,13 +3,26 @@ import { Context } from "../store/appContext";
 
 import { Button } from "../components/Button/button.jsx";
 
+// IMPORT ICONS
+import Icon from "../components/icons/icon.jsx";
+import { IconArrowLeft } from "@tabler/icons-react";
+
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<div className="text-center mt-5">
 			<h1 className="text-primary">Ya funciona</h1>
-			<Button>Hola</Button>
+			<Button>
+				<Icon name="LOGOUT" color="white" />
+			</Button>
+			<div className="d-flex justify-content-center align-content-center m-auto">
+				<button className="btn btn-secondary mt-3 d-flex justify-content-center align-content-center">
+					<Icon name="LOGOUT" />
+				</button>
+				<Icon name="LOGOUT" size="40" />
+				<IconArrowLeft color="red" size={48} />
+			</div>
 		</div>
 	);
 };
