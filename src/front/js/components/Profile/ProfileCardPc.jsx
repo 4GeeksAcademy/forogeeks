2cfdf6180cde86bbe016c90f962841baf79c3f00
Diego Gómez González <93382813+diegogomezgonza@users.noreply.github.com>
2024-03-07 12:43:46 +0100

    ProfileConfigurationPc actualizado

--- src/front/js/components/Profile/ProfileCardPc.jsx
+++ src/front/js/components/Profile/ProfileCardPc.jsx
@@ -5,7 +5,7 @@ const ProfileCardPc = ({ handleShowEmail, handleShowPassword }) => {
   return (
     <div className="col-md-4 mb-3 mb-md-0 d-flex justify-content-center align-items-center">
       {/* Contenedor del perfil en versión de escritorio */}
-      <div className="shadow rounded-4 mb-4 p-3 w-100 profile-container">
+      <div className="shadow rounded-4 mb-4 p-3 w-100 profile-container-card">
         {/* Icono de usuario y opción para subir foto */}
         {/* Título editar */}
         <div className="d-flex mt-2">
@@ -13,12 +13,12 @@ const ProfileCardPc = ({ handleShowEmail, handleShowPassword }) => {
         </div>
         <hr className="separator" />
         {/* Sección para cambiar email y contraseña */}
-        <div>
+        <div className="user-option">
           <p className="mb-0 ml-2 mb-2" onClick={handleShowEmail}>
             Cambiar email
           </p>
         </div>
-        <div>
+        <div className="user-option">
           <p className="mb-0 ml-2 mb-2" onClick={handleShowPassword}>
             Cambiar contraseña
           </p>
