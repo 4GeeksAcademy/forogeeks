679a0daf47da48acc4d28ce462c03e2efc761aaf
Diego Gómez González <93382813+diegogomezgonza@users.noreply.github.com>
2024-03-07 15:45:13 +0100

    Fix aspecto de profile

--- src/front/js/components/Profile/ProfileCardPc.jsx
+++ src/front/js/components/Profile/ProfileCardPc.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { IconUserCircle, IconUpload, IconPencil } from "@tabler/icons-react";
+import { IconLock, IconMail } from "@tabler/icons-react";
 
 const ProfileCardPc = ({ handleShowEmail, handleShowPassword }) => {
   return (
-    <div className="col-md-4 mb-3 mb-md-0 d-flex justify-content-center align-items-center">
+    <div className="col-md-4 mb-3 mb-md-0 d-flex justify-content-start align-items-start">
       {/* Contenedor del perfil en versión de escritorio */}
       <div className="shadow rounded-4 mb-4 p-3 w-100 profile-container-card">
         {/* Icono de usuario y opción para subir foto */}
@@ -13,12 +13,14 @@ const ProfileCardPc = ({ handleShowEmail, handleShowPassword }) => {
         </div>
         <hr className="separator" />
         {/* Sección para cambiar email y contraseña */}
-        <div className="user-option">
+        <div className="user-option d-flex">
+          <IconMail size={20} className="mt-1 mx-1"></IconMail>
           <p className="mb-0 ml-2 mb-2" onClick={handleShowEmail}>
             Cambiar email
           </p>
         </div>
-        <div className="user-option">
+        <div className="user-option d-flex">
+          <IconLock size={20} className="mt-1 mx-1"></IconLock>
           <p className="mb-0 ml-2 mb-2" onClick={handleShowPassword}>
             Cambiar contraseña
           </p>
