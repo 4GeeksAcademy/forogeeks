5f1e04a234e7b800f9e89c0f8d787c73c3152203
Diego Gómez González <93382813+diegogomezgonza@users.noreply.github.com>
2024-03-07 11:23:36 +0100

    ProfileCardPc actualizado

--- src/front/js/components/Profile/ProfileCardPc.jsx
+++ src/front/js/components/Profile/ProfileCardPc.jsx
@@ -5,21 +5,11 @@ const ProfileCardPc = ({ handleShowEmail, handleShowPassword }) => {
   return (
     <div className="col-md-4 mb-3 mb-md-0 d-flex justify-content-center align-items-center">
       {/* Contenedor del perfil en versión de escritorio */}
-      <div className="shadow rounded-4 mb-4 p-3 text-center w-100 profile-container">
+      <div className="shadow rounded-4 mb-4 p-3 w-100 profile-container">
         {/* Icono de usuario y opción para subir foto */}
-        <div className="d-flex align-items-center justify-content-center mt-3">
-          <IconUserCircle size={80} strokeWidth={1} />
-          <IconUpload size={25} strokeWidth={2} />
-        </div>
-        {/* Nombre de usuario editable */}
-        <div className="d-flex align-items-center justify-content-center mt-2">
-          <p className="mb-0 ml-2 mx-2 fw-bold">username</p>
-          <IconPencil size={20} strokeWidth={1} />
-        </div>
-        {/* Descripción editable */}
-        <div className="d-flex align-items-center justify-content-center mb-3">
-          <p className="mb-0 ml-2 mx-2 fw-bold">Me gusta react y JS.</p>
-          <IconPencil size={20} strokeWidth={1} />
+        {/* Título editar */}
+        <div className="d-flex mt-2">
+          <h1 className="display-4">Editar</h1>
         </div>
         <hr className="separator" />
         {/* Sección para cambiar email y contraseña */}
