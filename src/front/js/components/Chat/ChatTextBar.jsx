8d7f81e4889c45e8e99795854a3d5f9f4d522f13
Diego Gómez González <93382813+diegogomezgonza@users.noreply.github.com>
2024-03-08 13:28:39 +0100

    Chat distribuido en componentes

---
+++ src/front/js/components/Chat/ChatTextBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+const ChatTextBar = () => {
+
+    return (
+        <div className="chat-textbar-container">
+          <textarea
+            className="chat-textbar form-control"
+            rows="3"
+          ></textarea>
+        </div>
+    );
+};
+
+export default ChatTextBar;
