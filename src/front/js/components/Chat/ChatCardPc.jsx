0e04ccf6ea7c8a504b53f7b7fb2391e6482122c7
Diego Gómez González <93382813+diegogomezgonza@users.noreply.github.com>
2024-03-08 14:55:09 +0100

    Fix responsive 1

--- src/front/js/components/Chat/ChatCardPc.jsx
+++ src/front/js/components/Chat/ChatCardPc.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import { IconMail, IconMessageCircle2 } from "@tabler/icons-react";
 
-const ChatCardPc = ({ user, title, profileImg }) => {
+const ChatCardPc = ({ user, title, profileImg, messages }) => {
     return (
-        <div className="chat p-3 border rounded d-flex justify-content-between">
-            <div>
-                <div className="d-flex">
-                    <img className="chat-img" src={profileImg} alt="" />
-                    <h5 className="ms-1 mt-3">{title}</h5>
-                </div>
-
-                <p className="fw-bold">{user}</p>
-            </div>
-            <div>
-                <div className="d-flex">
-                    <IconMessageCircle2 className="mt-3"/>
-                </div>
-            </div>
+        <div className="chat-card-pc p-3 border rounded d-flex justify-content-between">
+        <div className="d-flex align-items-center">
+          <img className="chat-img-pc" src={profileImg} alt="" />
+          <div className="ms-2 mt-3">
+            <h6>{title}</h6>
+            <p className="text-muted small">{user}</p>
+          </div>
         </div>
+        <div>
+          <div className="d-flex">
+            <IconMessageCircle2 size={20} className="mt-3" />
+            <p>{messages}</p>
+          </div>
+        </div>
+      </div>
     );
 };
 
