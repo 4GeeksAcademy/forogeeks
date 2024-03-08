959c53aeef41fbaed272293dae8513c52cbe95e3
Raul M <118123543+pillow3@users.noreply.github.com>
2024-03-08 18:51:45 +0000

    Changes

--- src/front/js/pages/inside-thread.jsx
+++ src/front/js/pages/inside-thread.jsx
@@ -3,7 +3,12 @@ import { Link } from "react-router-dom";
 
 import { ThreadMessage } from "../components/Thread/threadMessage.jsx";
 import { ThreadParentMessage } from "../components/Thread/threadParentMessage.jsx";
+import { TextEditor } from "../components/TextEditor/text-editor.jsx";
+
+
+// ICONOS
 import { IconSquareRoundedPlus } from '@tabler/icons-react';
+
 
 export const InsideThread = () => {
     const contentTest = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
@@ -28,7 +33,13 @@ export const InsideThread = () => {
                         <ThreadMessage content={contentTest2} autor="@username09" date="Fecha del hilo" likes="5" profileImg="https://www.w3schools.com/howto/img_avatar.png" />
 
                     </div>
-
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="shadow-sm rounded-3 mb-4 py-1 px-3 bg-white pb-6">
+                                <TextEditor />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
