0cd198fa680e0015ee930d47da199a413e2ac6fa
Diego Gómez González <93382813+diegogomezgonza@users.noreply.github.com>
2024-03-09 11:19:55 +0100

    Fix modal

--- src/front/js/components/Modal/modalRegister.jsx
+++ src/front/js/components/Modal/modalRegister.jsx
@@ -1,72 +1,46 @@
 
-import { IconUser } from "@tabler/icons-react";
 import React from "react";
 
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-
 export const ModalRegister = ({ showRegister, handleCloseRegister }) => {
-
     return (
-
         <Modal show={showRegister} onHide={handleCloseRegister}>
-            <Modal.Header closeButton>
-           
-            </Modal.Header>
+            <Modal.Header closeButton> </Modal.Header>
             <Modal.Body className="ModalBody m-auto">
-                <div className="m-auto">
+                <div className="ContentBody  m-auto">
+                    <form className="formRegister">
+                        <p className="ContentBody titleR ">Register </p>
+                        <div className="Inputs">
+                            <div className="ContentName">
+                                <input required="" placeholder="Manuel" type="text" className="inputSignUpandRegister w-50" />
+                            </div>
+                            <div className="ContentLastname">
+                                <input required="" placeholder="Lastname" type="text" className="inputSignUpandRegister w-50" />
 
-
-                <form className="form">
-    <p className="title">Register </p>
-  
-        <div className="flex">
-        <label>
-            <input required="" placeholder="" type="text" className="input"/>
-            <span>Firstname</span>
-        </label>
-
-        <label>
-            <input required="" placeholder="" type="text" className="input"/>
-            <span>Lastname</span>
-        </label>
-    </div>  
-            
-    <label>
-        <input required="" placeholder="" type="email" className="input"/>
-        <span>Email</span>
-    </label> 
-        
-    <label>
-        <input required="" placeholder="" type="password" className="input"/>
-        <span>Password</span>
-    </label>
-    <label>
-        <input required="" placeholder="" type="password" className="input"/>
-        <span>Confirm password</span>
-    </label>
-    <button className="submit">Submit</button>
-    <p className="signin">Already have an acount ? <a href="#">Signin</a> </p>
-</form>
+                            </div>
+                            <div className="ContentEmail group ">
+                                <input required="" placeholder="email" type="email" className="inputSignUpandRegister" />
+                            </div>
+                            <div className="ContentPassword group ">
+                                <input required="" placeholder=" Password" type="password" className="inputSignUpandRegister" />
+                            </div>
+                            <div className=" group">
+                                <input required="  " placeholder="Confirm password" type="password" className="inputSignUpandRegister " />
+                            </div>
+                            <button className="submit">Submit</button>
+                            <p className="signin">Already have an acount ? <a href="#">Signin</a> </p>
+                        </div>
+                    </form>
                 </div>
 
 
             </Modal.Body>
             <Modal.Footer>
 
-            <button classNameName="Signup m-auto">
-                        <span classNameName="m-auto"></span>
-                        <div className="arrow-wrapper">
-                            <div className="arrow"></div>
+                <button type="submit" className="ButtonModal m-auto" > <span className="m-auto">Sign up</span>  <div className="arrow-wrapper"> <div className="arrow"></div>   </div> </button>
 
-                        </div>
-                    </button>
-
-
-                <Button variant="secondary" onClick={handleCloseRegister}>
-
-                </Button>
 
             </Modal.Footer>
         </Modal>
