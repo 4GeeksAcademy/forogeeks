04307d9cf7d51456a155f559ac0f1226ad5d68c1
Diego Gómez González <93382813+diegogomezgonza@users.noreply.github.com>
2024-03-12 22:33:51 +0000

    Merge

--- src/front/js/components/Profile/ProfileConfigurationPc.jsx
+++ src/front/js/components/Profile/ProfileConfigurationPc.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useContext } from "react";
 import { Context } from "../../store/appContext.js";
 
-const ProfileConfigurationPc = ({ showPassword }) => {
+const ProfileConfigurationPc = ({ showPassword,showEmail }) => {
   const { actions, store } = useContext(Context);
   const [newPassword, setNewPassword] = useState("");
   const [confirmNewPassword, setConfirmNewPassword] = useState("");
+  const [newEmail, setNewEmail] = useState("");
+  const [confirmNewEmail, setConfirmNewEmail] = useState("");
   const [alertMessage, setAlertMessage] = useState("");
   const [alertType, setAlertType] = useState("");
 
@@ -44,6 +46,42 @@ const ProfileConfigurationPc = ({ showPassword }) => {
     }
 };
 
+const handleSubmitEmail = async (e) => {
+  e.preventDefault();
+  // Validar que las contraseñas coincidan
+  if (newEmail !== confirmNewEmail) {
+      setAlertMessage("Los correos no coinciden.");
+      setAlertType("danger");
+      return;
+  }
+
+  try {
+      // Obtener el token del estado
+      const token = store.token;
+
+      // Enviar la solicitud para cambiar la contraseña utilizando el token obtenido
+      const { success, error } = await actions.changePassword(token, newEmail);
+      
+      if (success) {
+          // Mostrar alerta de éxito
+          setAlertMessage("Tu correo se ha actualizado.");
+          setAlertType("success");
+          // Vaciar los campos de entrada después de un envío exitoso
+          setNewEmail("");
+          setConfirmNewEmail("");
+      } else {
+          // Mostrar alerta de error
+          setAlertMessage(error || "Error al cambiar el correo. Por favor, inténtalo de nuevo.");
+          setAlertType("danger");
+      }
+  } catch (error) {
+      // Manejar cualquier error que ocurra al obtener el token del usuario
+      console.error("Error al cambiar el correo:", error);
+      setAlertMessage("Error al cambiar el correo. Por favor, inténtalo de nuevo.");
+      setAlertType("danger");
+  }
+};
+
   return (
     <div className="col-md-8">
       {/* Contenedor de configuración del perfil en versión de escritorio */}
@@ -76,6 +114,41 @@ const ProfileConfigurationPc = ({ showPassword }) => {
               </div>
               <button className="btn btn-primary text-white" onClick={handleSubmit}>
                 Cambiar Contraseña
+              </button>
+              {/* Mostrar alerta si hay mensaje */}
+              {alertMessage && (
+                <div className={`alert alert-${alertType}`} role="alert">
+                  {alertMessage}
+                </div>
+              )}
+            </>
+          )}
+          {showEmail && (
+            <>
+              <div className="form-group mb-3 w-50">
+                <label htmlFor="newEmail">Nuevo email:</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="newEmail"
+                  value={newEmail}
+                  onChange={(e) => setNewEmail(e.target.value)}
+                />
+              </div>
+              <div className="form-group mb-3 w-50">
+                <label htmlFor="confirmNewEmail">
+                  Confirmar nuevo email
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="confirmNewEmail"
+                  value={confirmNewEmail}
+                  onChange={(e) => setConfirmNewEmail(e.target.value)}
+                />
+              </div>
+              <button className="btn btn-primary text-white" onClick={handleSubmitEmail}>
+                Cambiar Email
               </button>
               {/* Mostrar alerta si hay mensaje */}
               {alertMessage && (
