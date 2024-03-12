5f8c10ad5155bc75ff17f92720fb8da190bbb946
LinseNain <141765188+LinseNain@users.noreply.github.com>
2024-03-12 00:23:07 +0000

    background animation

--- src/front/js/components/Modal/modalLogin.jsx
+++ src/front/js/components/Modal/modalLogin.jsx
@@ -25,13 +25,13 @@ export const ModalLogin = ({ showLogin, handleCloseLogin }) => {
 
     const handleLogin = async (e) => {
         e.preventDefault();
-    
+
         try {
             setEmailError({ isError: false, message: "" });
             setPasswordError({ isError: false, message: "" });
-    
+
             let hasError = false; // Flag para verificar si hay errores
-    
+
             // Verificar si el correo electrónico es válido
             if (email === "") {
                 setEmailError({ isError: true, message: "El correo electrónico es requerido" });
@@ -41,13 +41,13 @@ export const ModalLogin = ({ showLogin, handleCloseLogin }) => {
                 setPasswordError({ isError: true, message: "La contraseña es requerida" });
                 hasError = true; // Set the flag if there's an error
             }
-    
+
             if (!hasError) {
                 actions.login(email, password)
-                
+
                 // Si el inicio de sesión es exitoso, navegar a la página principal
                 navigate("/");
-                
+
                 // Cerrar el modal después de la redirección
                 handleCloseLogin();
                 // No es recomendable recargar la página aquí, ya que el redireccionamiento ya se encarga de cargar la nueva página.
@@ -57,109 +57,127 @@ export const ModalLogin = ({ showLogin, handleCloseLogin }) => {
             // Manejar errores durante el inicio de sesión, por ejemplo, mostrar un mensaje de error en la interfaz de usuario.
         }
     };
-    
+
 
     return (
         <>
-            <Modal className="background-modal" show={showLogin} onHide={handleCloseLogin}>
-                <Modal.Header closeButton className="ModalHeader">  </Modal.Header>
-                <Modal.Body className="ModalBody p-0">
+            {showLogin && (
+                <React.Fragment>
+                    <div className="  area">
+                        <ul className="circles">
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <Modal className="background-modal" show={showLogin} onHide={handleCloseLogin}>
+                                <Modal.Header closeButton className="ModalHeader">  </Modal.Header>
+                                <Modal.Body className="ModalBody p-0">
 
-                    {/* TITULO MODAL LOGIN */}
-                    <div className="ContentBody container d-flex flex-column justifycontent-center">
+                                    {/* TITULO MODAL LOGIN */}
+                                    <div className="ContentBody container d-flex flex-column justifycontent-center">
 
-                        <div className="DivForm row">
+                                        <div className="DivForm row">
 
-                            <div className="group p-0">
+                                            <div className="group p-0">
 
-                                <form className="" onSubmit={handleLogin} >
-                                    <h3 className="titleR d-flex align-items-center mt-3">Iniciar sesión</h3>
+                                                <form className="" onSubmit={handleLogin} >
+                                                    <h3 className="titleR d-flex align-items-center mt-3">Iniciar sesión</h3>
 
-                                    <div className="d-flex justifycontent-center flex-column">
-                                        {/* INPUT EMAIL */}
-                                        <div className="group mt-2 ">
-                                            <IconMail stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" className="icon" strokeLinejoin="round" strokeLinecap="round" />
-                                            <input className="inputSignUpandRegister " type="email" placeholder="Correo electrónico" onChange={(e) => setEmail(e.target.value)} id="inputEmailLogin" aria-describedby="emailHelp" />
-                                        </div>
-                                        {emailError.isError && <span className="small" style={{ color: "red" }}>{emailError.message}</span>}
+                                                    <div className="d-flex justifycontent-center flex-column">
+                                                        {/* INPUT EMAIL */}
+                                                        <div className="group mt-2 ">
+                                                            <IconMail stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" className="icon" strokeLinejoin="round" strokeLinecap="round" />
+                                                            <input className="inputSignUpandRegister " type="email" placeholder="Correo electrónico" onChange={(e) => setEmail(e.target.value)} id="inputEmailLogin" aria-describedby="emailHelp" />
+                                                        </div>
+                                                        {emailError.isError && <span className="small" style={{ color: "red" }}>{emailError.message}</span>}
 
-                                        {/* INPUT PASSWORD */}
-                                        <div className="group mt-3 w-100">
-                                            <IconLock stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" className="icon" strokeLinejoin="round" strokeLinecap="round" />
-                                            <input className="inputSignUpandRegister" type="password" placeholder="Contraseña" onChange={(e) => setPassword(e.target.value)} id="inputPasswordLogin" />
-                                        </div>
-                                        {passwordError.isError && <span className="small" style={{ color: "red" }}>{passwordError.message}</span>}
+                                                        {/* INPUT PASSWORD */}
+                                                        <div className="group mt-3 w-100">
+                                                            <IconLock stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" className="icon" strokeLinejoin="round" strokeLinecap="round" />
+                                                            <input className="inputSignUpandRegister" type="password" placeholder="Contraseña" onChange={(e) => setPassword(e.target.value)} id="inputPasswordLogin" />
+                                                        </div>
+                                                        {passwordError.isError && <span className="small" style={{ color: "red" }}>{passwordError.message}</span>}
 
-                                        {/* FORGOT PASSWORD */}
-                                        <div className="m-auto mt-2">
-                                            <span className="text-muted small">¿Olvidaste tu{" "}<a className="txt2 col-4" href="#" style={{ textDecoration: "none" }}>contraseña</a>?</span>
+                                                        {/* FORGOT PASSWORD */}
+                                                        <div className="m-auto mt-2">
+                                                            <span className="text-muted small">¿Olvidaste tu{" "}<a className="txt2 col-4" href="#" style={{ textDecoration: "none" }}>contraseña</a>?</span>
 
-                                        </div>
+                                                        </div>
 
-                                        {/* BUTTON LOGIN */}
-                                        <button type="submit" className="buttonModal w-100 m-auto mt-4 ">
-                                            <span className="m-auto">Iniciar sesión</span>
-                                            <div className="arrow-wrapper">
-                                                <div className="arrow"></div>
+                                                        {/* BUTTON LOGIN */}
+                                                        <button type="submit" className="buttonModal w-100 m-auto mt-4 ">
+                                                            <span className="m-auto">Iniciar sesión</span>
+                                                            <div className="arrow-wrapper">
+                                                                <div className="arrow"></div>
+                                                            </div>
+                                                        </button>
+
+                                                        {/* FORMAS DE SIGN IN */}
+                                                        <div className="text-center mt-3 d-flex flex-column justify-content-between">
+                                                            <div className="text-center mt-2 d-flex align-items-center justify-content-between">
+                                                                <div className="border-top flex-grow-1 mx-2"></div>
+                                                                <span className="mx-2 text-muted">o</span>
+                                                                <div className="border-top flex-grow-1 mx-2"></div>
+                                                            </div>
+
+
+                                                            {/* INICIA CON GITHUB */}
+
+                                                            <button type="submit" className=" w-100 m-auto mt-4 mb-3 btn btn-dark rounded-4">
+                                                                <span className="IconGithub me-2"><IconBrandGithub /></span>
+                                                                Inicia con GitHub
+                                                                <div className="arrow-wrapper">
+                                                                    <div className="arrow"></div>
+                                                                </div>
+                                                            </button>
+
+                                                        </div>
+
+                                                        {/* REGISTER */}
+                                                        <div className="text-center mt-2 mb-4 px-2">
+                                                            <span>¿Aún no tienes una cuenta? </span>
+                                                            <a className="" style={{ textDecoration: "none" }} href="#" onClick={() => { handleShowRegister(); handleCloseLogin(); }} >
+                                                                Regístrate gratis
+                                                            </a>
+
+                                                        </div>
+
+
+
+                                                    </div>
+
+
+
+                                                </form>
+
+
                                             </div>
-                                        </button>
-
-                                        {/* FORMAS DE SIGN IN */}
-                                        <div className="text-center mt-3 d-flex flex-column justify-content-between">
-                                            <div className="text-center mt-2 d-flex align-items-center justify-content-between">
-                                                <div className="border-top flex-grow-1 mx-2"></div>
-                                                <span className="mx-2 text-muted">o</span>
-                                                <div className="border-top flex-grow-1 mx-2"></div>
-                                            </div>
-
-
-                                            {/* INICIA CON GITHUB */}
-
-                                            <button type="submit" className=" w-100 m-auto mt-4 mb-3 btn btn-dark rounded-4">
-                                                <span className="IconGithub me-2"><IconBrandGithub /></span>
-                                                Inicia con GitHub
-                                                <div className="arrow-wrapper">
-                                                    <div className="arrow"></div>
-                                                </div>
-                                            </button>
 
                                         </div>
-
-                                        {/* REGISTER */}
-                                        <div className="text-center mt-2 mb-4 px-2">
-                                            <span>¿Aún no tienes una cuenta? </span>
-                                            <a className="" style={{ textDecoration: "none" }} href="#" onClick={() => { handleShowRegister(); handleCloseLogin(); }} >
-                                                Regístrate gratis
-                                            </a>
-
-                                        </div>
-
-
 
                                     </div>
 
 
 
-                                </form>
 
 
-                            </div>
 
-                        </div>
 
+                                </Modal.Body>
+
+                            </Modal>
+                              
+            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
                     </div>
 
+                </React.Fragment >
+            )}
 
-
-
-
-
-
-                </Modal.Body>
-
-            </Modal>
             {/* Modal de registro */}
             < ModalRegister showRegister={showRegister} handleCloseRegister={handleCloseRegister} />
+          
         </>
     );
 };
--- src/front/js/components/Modal/modalRegister.jsx
+++ src/front/js/components/Modal/modalRegister.jsx
@@ -83,95 +83,111 @@ export const ModalRegister = ({ showRegister, handleCloseRegister }) => {
 
     return (
         <>
-            <Modal className="background-modal" show={showRegister} onHide={handleCloseRegister}>
-                <Modal.Header closeButton className="ModalHeader" />
-                <Modal.Body className="ModalBody p-0">
+            {showRegister && (
+                <React.Fragment>
+                    <div className="  area">
+                        <ul className="circles">
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <Modal className="background-modal " show={showRegister} onHide={handleCloseRegister}>
+                                <Modal.Header closeButton className="ModalHeader" />
+                                <Modal.Body className="ModalBody p-0">
 
-                    {/* TITULO MODAL REGISTRO */}
-                    <div className="ContentBody container d-flex flex-column justifycontent-center">
+                                    {/* TITULO MODAL REGISTRO */}
+                                    <div className="ContentBody container d-flex flex-column justifycontent-center">
 
-                        <div className="DivForm row">
+                                        <div className="DivForm row">
 
-                            <div className="group p-0">
+                                            <div className="group p-0">
 
-                                <form className="form" >
-                                    <h3 className="titleR d-flex align-items-center mt-3">Crea tu cuenta</h3>
+                                                <form className="form" >
+                                                    <h3 className="titleR d-flex align-items-center mt-3">Crea tu cuenta</h3>
 
-                                    <div className="d-flex justifycontent-center flex-column">
-                                        {/* INPUT USERNAME */}
-                                        <div className="group mt-2 ">
-                                            <IconUser stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" className="icon" strokeLinejoin="round" strokeLinecap="round" />
-                                            <input className="inputSignUpandRegister" type="text" placeholder="Nombre de usuario" onChange={(e) => setUsername(e.target.value)} />
-                                        </div>
-                                        {usernameError.isError && <span className="small" style={{ color: "red" }}>{usernameError.message}</span>}
+                                                    <div className="d-flex justifycontent-center flex-column">
+                                                        {/* INPUT USERNAME */}
+                                                        <div className="group mt-2 ">
+                                                            <IconUser stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" className="icon" strokeLinejoin="round" strokeLinecap="round" />
+                                                            <input className="inputSignUpandRegister" type="text" placeholder="Nombre de usuario" onChange={(e) => setUsername(e.target.value)} />
+                                                        </div>
+                                                        {usernameError.isError && <span className="small" style={{ color: "red" }}>{usernameError.message}</span>}
 
-                                        {/* INPUT EMAIL */}
-                                        <div className="group mt-3 ">
-                                            <IconMail stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" className="icon" strokeLinejoin="round" strokeLinecap="round" />
-                                            <input className="inputSignUpandRegister" type="email" placeholder="Correo electrónico" onChange={(e) => setEmail(e.target.value)} />
-                                        </div>
-                                        {emailError.isError && <span className="small" style={{ color: "red" }}>{emailError.message}</span>}
+                                                        {/* INPUT EMAIL */}
+                                                        <div className="group mt-3 ">
+                                                            <IconMail stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" className="icon" strokeLinejoin="round" strokeLinecap="round" />
+                                                            <input className="inputSignUpandRegister" type="email" placeholder="Correo electrónico" onChange={(e) => setEmail(e.target.value)} />
+                                                        </div>
+                                                        {emailError.isError && <span className="small" style={{ color: "red" }}>{emailError.message}</span>}
 
-                                        {/* INPUT PASSWORD */}
-                                        <div className="group mt-3 w-100">
-                                            <IconLock stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" className="icon" strokeLinejoin="round" strokeLinecap="round" />
-                                            <input className="inputSignUpandRegister" type="password" placeholder="Contraseña" onChange={(e) => setPassword(e.target.value)} />
-                                        </div>
-                                        {passwordError.isError && <span className="small" style={{ color: "red" }}>{passwordError.message}</span>}
+                                                        {/* INPUT PASSWORD */}
+                                                        <div className="group mt-3 w-100">
+                                                            <IconLock stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" className="icon" strokeLinejoin="round" strokeLinecap="round" />
+                                                            <input className="inputSignUpandRegister" type="password" placeholder="Contraseña" onChange={(e) => setPassword(e.target.value)} />
+                                                        </div>
+                                                        {passwordError.isError && <span className="small" style={{ color: "red" }}>{passwordError.message}</span>}
 
 
-                                        {/* CONFIRM PASSWORD */}
-                                        <div className="group mt-3 w-100">
-                                            <IconLock stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" className="icon" strokeLinejoin="round" strokeLinecap="round" />
-                                            <input className="inputSignUpandRegister" type="password" placeholder="Confirmar contraseña" onChange={(e) => setConfirmPassword(e.target.value)} />
-                                        </div>
-                                        {passwordError.isError && <span className="small" style={{ color: "red" }}>{passwordError.message}</span>}
+                                                        {/* CONFIRM PASSWORD */}
+                                                        <div className="group mt-3 w-100">
+                                                            <IconLock stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" className="icon" strokeLinejoin="round" strokeLinecap="round" />
+                                                            <input className="inputSignUpandRegister" type="password" placeholder="Confirmar contraseña" onChange={(e) => setConfirmPassword(e.target.value)} />
+                                                        </div>
+                                                        {passwordError.isError && <span className="small" style={{ color: "red" }}>{passwordError.message}</span>}
 
-                                        {/* BUTTON REGISTER */}
-                                        <button type="button" onClick={handleSignup} className="buttonModal w-100 m-auto mt-4" >
-                                            <span className="m-auto">Registrarse</span>
-                                            <div className="arrow-wrapper">
-                                                <div className="arrow"></div>
+                                                        {/* BUTTON REGISTER */}
+                                                        <button type="button" onClick={handleSignup} className="buttonModal w-100 m-auto mt-4" >
+                                                            <span className="m-auto">Registrarse</span>
+                                                            <div className="arrow-wrapper">
+                                                                <div className="arrow"></div>
+                                                            </div>
+                                                        </button>
+
+                                                        {/* INICIA CON GITHUB */}
+                                                        <div className="text-center mt-3 d-flex flex-column justify-content-between">
+                                                            <div className="text-center mt-2 d-flex align-items-center justify-content-between">
+                                                                <div className="border-top flex-grow-1 mx-2"></div>
+                                                                <span className="mx-2 text-muted">o</span>
+                                                                <div className="border-top flex-grow-1 mx-2"></div>
+                                                            </div>
+
+                                                            <button type="submit" className="w-100 m-auto mt-4 mb-3 btn btn-dark rounded-4">
+                                                                <span className="IconGithub me-2"><IconBrandGithub /></span>
+                                                                Inicia con GitHub
+                                                                <div className="arrow-wrapper">
+                                                                    <div className="arrow"></div>
+                                                                </div>
+                                                            </button>
+                                                        </div>
+
+                                                        {/* SIGN IN LINK */}
+                                                        <div className="text-center mt-2 mb-4">
+                                                            <span>¿Ya tienes una cuenta? </span>
+                                                            <a className="" style={{ textDecoration: "none" }} href="#" onClick={() => { handleShowLogin(); handleCloseRegister(); }} >
+                                                                Iniciar sesión
+                                                            </a>
+                                                        </div>
+
+                                                    </div>
+
+                                                </form>
+
+
                                             </div>
-                                        </button>
 
-                                        {/* INICIA CON GITHUB */}
-                                        <div className="text-center mt-3 d-flex flex-column justify-content-between">
-                                            <div className="text-center mt-2 d-flex align-items-center justify-content-between">
-                                                <div className="border-top flex-grow-1 mx-2"></div>
-                                                <span className="mx-2 text-muted">o</span>
-                                                <div className="border-top flex-grow-1 mx-2"></div>
-                                            </div>
-
-                                            <button type="submit" className="w-100 m-auto mt-4 mb-3 btn btn-dark rounded-4">
-                                                <span className="IconGithub me-2"><IconBrandGithub /></span>
-                                                Inicia con GitHub
-                                                <div className="arrow-wrapper">
-                                                    <div className="arrow"></div>
-                                                </div>
-                                            </button>
-                                        </div>
-
-                                        {/* SIGN IN LINK */}
-                                        <div className="text-center mt-2 mb-4">
-                                            <span>¿Ya tienes una cuenta? </span>
-                                            <a className="" style={{ textDecoration: "none" }} href="#" onClick={() => { handleShowLogin(); handleCloseRegister(); }} >
-                                                Iniciar sesión
-                                            </a>
                                         </div>
 
                                     </div>
 
-                                </form>
-
-                            </div>
-
-                        </div>
-
+                                </Modal.Body>
+                            </Modal>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
                     </div>
 
-                </Modal.Body>
-            </Modal>
+                </React.Fragment >
+            )}
 
             {/* <ModalLogin showLogin={showLogin} handleCloseLogin={handleCloseLogin}></ModalLogin> */}
 
