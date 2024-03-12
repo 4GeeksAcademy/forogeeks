01bccd5ec26cd5e13bbaff36d447bfc7436858c2
LinseNain <141765188+LinseNain@users.noreply.github.com>
2024-03-12 15:15:50 +0000

    haciendo search

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,33 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			// categories: [
-			// 	{
-			// 		name: "General",
-			// 		icon: "IconMessages",
-			// 	},
-			// 	{
-			// 		name: "Tecnología",
-			// 		icon: "IconDeviceLaptop",
-			// 	},
-			// 	{
-			// 		name: "Snippets de código",
-			// 		icon: "IconCode",
-			// 	},
-			// 	{
-			// 		name: "Empleo y prácticas",
-			// 		icon: "IconNotes",
-			// 	},
-			// 	{
-			// 		name: "Videojuegos",
-			// 		icon: "IconDeviceGamepad2",
-			// 	},
-			// 	{
-			// 		name: "Info / Ayuda",
-			// 		icon: "IconHelp",
-			// 	},
-			// ],
-
+		
 			logError: null,
 			token: "",
 			modalRegistersuccess: false,
