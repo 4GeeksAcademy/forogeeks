ca8fe35984f88592ed458852f7420618fe963daa
Raul Cano <118123543+raulcanodev@users.noreply.github.com>
2024-03-13 00:52:53 +0000

    Trending

--- src/front/js/components/trending.jsx
+++ src/front/js/components/trending.jsx
@@ -26,7 +26,7 @@ const AsideTrending = () => {
 	return (
 		<div className="shadow-sm rounded-3 mb-2 bg-white">
 			<div className="d-flex flex-column ps-3 pt-3 pb-0">
-				<div className="d-flex flex-row p-0">
+				<div className="d-flex flex-row pt-2 ps-1">
 					<Icon name="FLAME" size={25} color="currentColor" />
 					<h4 className="p-0 m-0">Trending</h4>
 				</div>
@@ -34,8 +34,10 @@ const AsideTrending = () => {
 					<span className="text-muted small ps-1">Los hilos del momento</span>
 				</div>
 			</div>
+
 			<div>
-				<div className="py-3">
+				<div className="pb-3">
+			<hr className="hr mb-0 mx-2" style={{ opacity: "10%" }}></hr>
 					{trending &&
 						// Mapear TrendingThreads por cada thread
 						trending.slice(0, 5).map((thread, index) => (
@@ -45,7 +47,7 @@ const AsideTrending = () => {
 								title={thread.title}
 								likes={thread.likes}
 								number_of_comments={thread?.thread_comments?.length}
-								autor={thread.autor}
+								author={thread.user.user_name}
 								date={thread.date}
 								category={thread.category}
 								id={thread.id}
