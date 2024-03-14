f5cfa0f936454856bb773756a378c12d0b967db1
Raul Cano <118123543+raulcanodev@users.noreply.github.com>
2024-03-14 21:01:01 +0000

    quitar imagen 2

--- webpack.common.js
+++ webpack.common.js
@@ -49,7 +49,7 @@ module.exports = {
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			favicon: "forogeeks.jpeg",
+			favicon: "",
 			template: "template.html",
 		}),
 		new Dotenv({ safe: true, systemvars: true }),
