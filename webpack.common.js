3b6ccf239299bafe2bba32f11ac6017b04f86186
Raul M <118123543+pillow3@users.noreply.github.com>
2024-03-14 21:42:14 +0000

    favicon

--- webpack.common.js
+++ webpack.common.js
@@ -49,7 +49,7 @@ module.exports = {
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			favicon: "",
+			favicon: "4geeks.ico",
 			template: "template.html",
 		}),
 		new Dotenv({ safe: true, systemvars: true }),
