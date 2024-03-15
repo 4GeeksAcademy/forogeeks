39a20cd278c6a8163e0d06d815af069c74242990
Diego Gómez González <93382813+diegogomezgonza@users.noreply.github.com>
2024-03-15 03:29:13 +0000

    modales actualizados

--- src/front/js/components/Modal/ModalProfile.jsx
+++ src/front/js/components/Modal/ModalProfile.jsx
@@ -1,6 +1,7 @@
 // ModalProfile.jsx
 import React, { useState, useContext, useEffect } from "react";
 import { Context } from "../../store/appContext";
+import { IconPassword } from "@tabler/icons-react";
 const ModalProfile = ({
   // Indica si el modal debe mostrarse o no
   show,
@@ -24,38 +25,38 @@ const ModalProfile = ({
     e.preventDefault();
     // Validar que las contraseñas coincidan
     if (newPassword !== confirmNewPassword) {
-        setAlertMessage("Las contraseñas no coinciden.");
-        setAlertType("danger");
-        return;
+      setAlertMessage("Las contraseñas no coinciden.");
+      setAlertType("danger");
+      return;
     }
 
     try {
-        // Obtener el token del estado
-        const token = store.token;
-  
-        // Enviar la solicitud para cambiar la contraseña utilizando el token obtenido
-        const { success, error } = await actions.changePassword(token, newPassword);
-        
-        if (success) {
-            // Mostrar alerta de éxito
-            setAlertMessage("Tu contraseña se ha actualizado.");
-            setAlertType("success");
-            // Vaciar los campos de entrada después de un envío exitoso
-            updateChangesSaved(true);
-            setNewPassword("");
-            setConfirmNewPassword("");
-        } else {
-            // Mostrar alerta de error
-            setAlertMessage(error || "Error al cambiar la contraseña. Por favor, inténtalo de nuevo.");
-            setAlertType("danger");
-        }
-    } catch (error) {
-        // Manejar cualquier error que ocurra al obtener el token del usuario
-        console.error("Error al cambiar la contraseña:", error);
-        setAlertMessage("Error al cambiar la contraseña. Por favor, inténtalo de nuevo.");
+      // Obtener el token del estado
+      const token = store.token;
+
+      // Enviar la solicitud para cambiar la contraseña utilizando el token obtenido
+      const { success, error } = await actions.changePassword(token, newPassword);
+
+      if (success) {
+        // Mostrar alerta de éxito
+        setAlertMessage("Tu contraseña se ha actualizado.");
+        setAlertType("success");
+        // Vaciar los campos de entrada después de un envío exitoso
+        updateChangesSaved(true);
+        setNewPassword("");
+        setConfirmNewPassword("");
+      } else {
+        // Mostrar alerta de error
+        setAlertMessage(error || "Error al cambiar la contraseña. Por favor, inténtalo de nuevo.");
         setAlertType("danger");
+      }
+    } catch (error) {
+      // Manejar cualquier error que ocurra al obtener el token del usuario
+      console.error("Error al cambiar la contraseña:", error);
+      setAlertMessage("Error al cambiar la contraseña. Por favor, inténtalo de nuevo.");
+      setAlertType("danger");
     }
-};
+  };
 
   return (
     <div
@@ -64,10 +65,12 @@ const ModalProfile = ({
       aria-labelledby="passwordModalLabel"
       aria-hidden={!show}
     >
+      {/* Modal*/}
       <div className="modal-dialog modal-dialog-centered">
-        <div className="modal-content">
+        {/* Modal Content*/}
+        <div className="modal-content p-3 m-auto">
+          {/* Modal Header*/}
           <div className="modal-header">
-            <h5 className="modal-title">{title}</h5>
             <button
               type="button"
               className="btn-close"
@@ -75,40 +78,60 @@ const ModalProfile = ({
               aria-label="Close"
             ></button>
           </div>
-          <div className="form-group mb-3 w-50">
-                <label htmlFor="newPassword">Nueva contraseña:</label>
+          {/* Modal Body*/}
+          <div className="modalBody">
+            <div className="m-auto mt-2">
+              <p className=" fw-bold  pt-2  text-center  w-100" htmlFor="email">
+                Nueva contraseña:
+              </p>
+            </div>
+            <div className="mt-2">
+              <div className="form group w-75 m-auto" >
+                <IconPassword stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" className="icon" strokeLinejoin="round" strokeLinecap="round" />
                 <input
                   type="password"
-                  className="form-control"
+                  className="form-control  inputSignUpandRegister"
+                  placeholder="Contraseña"
                   id="newPassword"
                   value={newPassword}
                   onChange={(e) => setNewPassword(e.target.value)}
                 />
               </div>
-              <div className="form-group mb-3 w-50">
-                <label htmlFor="confirmNewPassword">
-                  Confirmar nueva contraseña:
-                </label>
-                <input
-                  type="password"
-                  className="form-control"
-                  id="confirmNewPassword"
-                  value={confirmNewPassword}
-                  onChange={(e) => setConfirmNewPassword(e.target.value)}
-                />
-              </div>
-              <button className="btn btn-primary text-white" onClick={handleSubmit}>
-                Cambiar Contraseña
-              </button>
-              {/* Mostrar alerta si hay mensaje */}
-              {alertMessage && (
-                <div className={`alert alert-${alertType}`} role="alert">
-                  {alertMessage}
+            </div>
+            <div className="m-auto mt-2">
+              <p className=" fw-bold  pt-2  text-center  w-100" htmlFor="email">
+                Confirmar nueva contraseña:
+              </p>
+            </div>
+            <div className="form group w-75 m-auto" >
+              <IconPassword stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" className="icon" strokeLinejoin="round" strokeLinecap="round" />
+              <input
+                type="password"
+                className="form-control  inputSignUpandRegister"
+                id="confirmNewPassword"
+                placeholder="Confirmar contraseña"
+                value={confirmNewPassword}
+                onChange={(e) => setConfirmNewPassword(e.target.value)}
+              />
+            </div>
+            {/* Modal Button*/}
+            <div className="mt-4 mb-2 ">
+              <button className="btn  btn-primary text-white  buttonModal m-auto  text-center" onClick={handleSubmit}>
+                Confirmar  <div className="arrow-wrapper">
+                  <div className="arrow"></div>
                 </div>
-              )}
+              </button>
+            </div>
+            {/* Mostrar alerta si hay mensaje */}
+            {alertMessage && (
+              <div className={`alert alert-${alertType}`} role="alert">
+                {alertMessage}
+              </div>
+            )}
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
--- src/front/js/components/Modal/ModalProfileEmail.jsx
+++ src/front/js/components/Modal/ModalProfileEmail.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import { Context } from "../../store/appContext.js";
-
+import { IconRecordMail } from "@tabler/icons-react";
 const ModalProfileEmail = ({ show, handleClose, title }) => {
   const { actions, store } = useContext(Context);
   const [newEmail, setNewEmail] = useState("");
@@ -12,71 +12,93 @@ const ModalProfileEmail = ({ show, handleClose, title }) => {
     e.preventDefault();
     // Validar que los correos electrónicos coincidan
     if (newEmail !== confirmNewEmail) {
-        setAlertMessage("Los correos no coinciden.");
-        setAlertType("danger");
-        return;
+      setAlertMessage("Los correos no coinciden.");
+      setAlertType("danger");
+      return;
     }
 
     try {
-        // Obtener el token del estado
-        const token = store.token;
+      // Obtener el token del estado
+      const token = store.token;
 
-        // Enviar la solicitud para cambiar el correo electrónico utilizando el token obtenido
-        const { success, error } = await actions.changeEmail(token, newEmail);
-        
-        if (success) {
-            // Mostrar alerta de éxito
-            setAlertMessage("Tu correo se ha actualizado. Inicia sesión con tu nuevo correo.");
-            setAlertType("success");
-            // Vaciar los campos de entrada después de un envío exitoso
-            setNewEmail("");
-            setConfirmNewEmail("");
-            actions.logout();
-        } else {
-            // Mostrar alerta de error
-            setAlertMessage(error || "Error al cambiar el correo. Por favor, inténtalo de nuevo.");
-            setAlertType("danger");
-        }
-    } catch (error) {
-        // Manejar cualquier error que ocurra al obtener el token del usuario
-        console.error("Error al cambiar el correo:", error);
-        setAlertMessage("Error al cambiar el correo. Por favor, inténtalo de nuevo.");
+      // Enviar la solicitud para cambiar el correo electrónico utilizando el token obtenido
+      const { success, error } = await actions.changeEmail(token, newEmail);
+
+      if (success) {
+        // Mostrar alerta de éxito
+        setAlertMessage("Tu correo se ha actualizado. Inicia sesión con tu nuevo correo.");
+        setAlertType("success");
+        // Vaciar los campos de entrada después de un envío exitoso
+        setNewEmail("");
+        setConfirmNewEmail("");
+        actions.logout();
+      } else {
+        // Mostrar alerta de error
+        setAlertMessage(error || "Error al cambiar el correo. Por favor, inténtalo de nuevo.");
         setAlertType("danger");
+      }
+    } catch (error) {
+      // Manejar cualquier error que ocurra al obtener el token del usuario
+      console.error("Error al cambiar el correo:", error);
+      setAlertMessage("Error al cambiar el correo. Por favor, inténtalo de nuevo.");
+      setAlertType("danger");
     }
-};
+  };
 
   return (
     <div className={`modal fade ${show ? "show" : ""}`} tabIndex="-1" aria-labelledby="emailModalLabel" aria-hidden={!show}>
+      {/* Modal */}
       <div className="modal-dialog modal-dialog-centered">
-        <div className="modal-content">
+        {/* Modal Content*/}
+        <div className="modal-content p-3 m-auto">
+          {/* Modal Header*/}
           <div className="modal-header">
-            <h5 className="modal-title">{title}</h5>
             <button type="button" className="btn-close" onClick={handleClose} aria-label="Close"></button>
           </div>
+          {/* Modal Body*/}
           <div className="modal-body">
-            <div className="form-group mb-3 w-50">
-              <label htmlFor="newEmail">Nuevo email:</label>
-              <input
-                type="email"
-                className="form-control"
-                id="newEmail"
-                value={newEmail}
-                onChange={(e) => setNewEmail(e.target.value)}
-              />
+            <div className="m-auto mt-2">
+              <p className=" fw-bold  pt-2  text-center  w-100" htmlFor="email">
+                Nuevo email:
+              </p>
             </div>
-            <div className="form-group mb-3 w-50">
-              <label htmlFor="confirmNewEmail">Confirmar nuevo email:</label>
+            <div className="mt-2">
+              <div className="form group w-75 m-auto" >
+                <IconRecordMail stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" className="icon" strokeLinejoin="round" strokeLinecap="round" />
+                <input
+                  type="email"
+                  className="form-control  inputSignUpandRegister"
+                  placeholder="Nuevo email"
+                  id="newEmail"
+                  value={newEmail}
+                  onChange={(e) => setNewEmail(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="m-auto mt-2">
+              <p className=" fw-bold  pt-2 text-center  w-100" htmlFor="email">
+                Confirmar nuevo email:
+              </p>
+            </div>
+            <div className="form group w-75 m-auto" >
+              <IconRecordMail stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" className="icon" strokeLinejoin="round" strokeLinecap="round" />
               <input
                 type="email"
-                className="form-control"
+                placeholder="Confirmar nuevo email"
+                className="form-control inputSignUpandRegister  "
                 id="confirmNewEmail"
                 value={confirmNewEmail}
                 onChange={(e) => setConfirmNewEmail(e.target.value)}
               />
             </div>
-            <button className="btn btn-primary text-white" onClick={handleSubmitEmail}>
-              Cambiar Email
-            </button>
+            {/* Modal Button*/}
+            <div className="mt-4 mb-2 ">
+              <button className="btn  btn-primary text-white  buttonModal m-auto  text-center" onClick={handleSubmitEmail}>
+                Confirmar  <div className="arrow-wrapper">
+                  <div className="arrow"></div>
+                </div>
+              </button>
+            </div >
             {/* Mostrar alerta si hay mensaje */}
             {alertMessage && (
               <div className={`alert alert-${alertType}`} role="alert">
