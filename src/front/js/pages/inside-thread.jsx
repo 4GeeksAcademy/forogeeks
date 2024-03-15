21f278b93618b705c3ad96675ee8dc4589f5ad6d
Diego Gómez González <93382813+diegogomezgonza@users.noreply.github.com>
2024-03-15 11:43:43 +0000

    Limpiar textbar al crear hilo

--- src/front/js/pages/inside-thread.jsx
+++ src/front/js/pages/inside-thread.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useContext } from "react";
 import { Context } from "../store/appContext";
